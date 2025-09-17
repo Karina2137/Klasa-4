578a73c73c935d564a648ce3681f0d8fa74e50ec
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2025-09-17 16:36:40 +0200

    Update zadania.py

--- Informatyka/01_Algorytm zachłanny/zadania.py
+++ Informatyka/01_Algorytm zachłanny/zadania.py
@@ -2,6 +2,7 @@ plik = open("nominaly.txt", "r")
 nominaly = list(map(int, plik.read().split()))
 plik.close()
 
+#zad.1
 print(nominaly)
 print("Z ilu wydać resztę?")
 kwota = int(input())
@@ -15,3 +16,16 @@ print(ile)
 for i in range (n):
     if ile[i]>0:
         print(ile[i]," x ",nominaly[i])
+
+#zad.5
+wartosc = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000, }
+rzymska = input()
+liczba = 0
+ostatnia = rzymska[-1]
+for i in range(len(rzymska)-1):
+    litera = rzymska[i]
+    nastepna = rzymska[i+1]
+    if wartosc[litera] >= wartosc[nastepna]: liczba += wartosc[litera]
+    else: liczba -= wartosc[litera]
+liczba+=wartosc[ostatnia]
+print(liczba)
