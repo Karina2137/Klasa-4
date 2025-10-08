9816b5384e49008e35ef7c434640c82d637c819d
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2025-10-08 16:15:30 +0200

    Create zadania.py

---
+++ Informatyka/02_najwieksza_suma_w_kwadracie/zadania.py
@@ -0,0 +1,58 @@
+#zad3
+M = [
+        [3, 7, 2, 6, 4],
+        [5, 1, 9, 3, 8],
+        [6, 2, 4, 7, 1],
+        [8, 3, 5, 2, 9],
+        [4, 6, 1, 5, 7]
+    ]
+for t in M:
+    print(t)
+
+n = len(M)
+i = 0
+j = 0
+suma = M[0][0]
+while(i < n - 1 or j < n - 1):
+    if (i == n - 1):
+        j += 1
+    elif (j == n - 1):
+        i += 1
+    elif (int(M[i + 1][j])> int(M[i][j + 1])):
+        i += 1
+    else:
+        j += 1
+    suma += int(M[i][j])
+
+print()
+for t in M:
+    print(t)
+print(suma)
+
+#zad4
+plik = open("macierz.txt", "r")
+M = []
+for line in plik:
+    M.append(list(map(int, line.split())))
+plik.close()
+
+n = len(M)
+i = 0
+j = 0
+suma = M[0][0]
+while(i < n - 1 or j < n - 1):
+    if (i == n - 1):
+        j += 1
+    elif (j == n - 1):
+        i += 1
+    elif (int(M[i + 1][j])> int(M[i][j + 1])):
+        i += 1
+    else:
+        j += 1
+    suma += int(M[i][j])
+
+print()
+for t in M:
+    print(t)
+print(suma)
+
